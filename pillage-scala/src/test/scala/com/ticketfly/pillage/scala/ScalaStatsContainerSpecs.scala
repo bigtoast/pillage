6d36c5547d7c9fc92dac361acb2625bfb876469d
Dragisa Krsmanovic <dragisak@ticketfly.com>
2014-08-14 13:37:37 -0700

    Upgrade NewRelic to version 3.9.0

--- pillage-scala/src/test/scala/com/ticketfly/pillage/scala/ScalaStatsContainerSpecs.scala
+++ pillage-scala/src/test/scala/com/ticketfly/pillage/scala/ScalaStatsContainerSpecs.scala
@@ -1,34 +1,34 @@
 package com.ticketfly.pillage.scala
 
 import org.scalatest.WordSpec
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.ShouldMatchers
 import com.ticketfly.pillage._
 
 class ScalaStatsContainerSpecs extends WordSpec with ShouldMatchers {
 
-  object PillageStats extends StatsContainerImpl( new HistogramMetricFactory ) with ScalaStatsContainer 
+  object PillageStats extends StatsContainerImpl( new HistogramMetricFactory ) with ScalaStatsContainer
   "testing in gradle" should {
     "freakin work" in {
       1 should be (1)
     }
   }
-  
+
   "The PillageStats object" should {
     "create some counters" in {
       PillageStats incr "test-counter"
       PillageStats incr("test-counter", 100)
-      
+
       PillageStats getCounter("test-counter") value() should be (101)
     }
-    
+
     "time some stuff" in {
       PillageStats.time[Unit]("test-timer"){
         Thread.sleep(500)
       }
-      
+
       PillageStats.metrics.get("test-timer.millis").getSum() should be >= (500L)
       PillageStats.metrics.get("test-timer.millis").getSum() should be < (1000L)
-      
+
     }
 
     "register a gauge with curried method" in {
@@ -43,7 +43,7 @@ class ScalaStatsContainerSpecs extends WordSpec with ShouldMatchers {
       d1 should not be (d2)
 
     }
-    
-    
+
+
   }
 }
